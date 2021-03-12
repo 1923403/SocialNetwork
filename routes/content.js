721057b91966663c57ad7ffce0bc1b7fb012dc00
mahehsma <maxi.hehl@gmail.com>
2021-03-12 15:43:18 +0100

    added login check

--- routes/content.js
+++ routes/content.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { isLoggedIn } = require('../middleware/users')
+const { isLoggedIn } = require("../middleware/users");
+const { requirementsFullfilled } = require("../middleware/content");
 const { createContent } = require("../controller/content");
 
 router
   .route("/")
   // .get(getContent)
-  .post(isLoggedIn, createContent);
+  .post(isLoggedIn, requirementsFullfilled, createContent);
 // .put(updateContent)
 // .delete(deleteContent);
 
