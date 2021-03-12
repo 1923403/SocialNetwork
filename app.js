8b8b726ddab856d82e61dc32ae8ef92f9729ba82
mahehsma <maxi.hehl@gmail.com>
2021-03-12 21:48:14 +0100

    added cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
 // Route files
 const contentRouter = require("./routes/content");
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
 app.use("/content", contentRouter);
 // app.use('/follow', followRouter);
